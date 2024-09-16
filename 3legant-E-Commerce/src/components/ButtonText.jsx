da49fe4d3b42bedfaf1c28834fb30d243a1123e8
Toshak Parmar <108329884+toshakparmar@users.noreply.github.com>
2024-09-16 17:37:40 +0530

    Update ButtonText.jsx

--- 3legant-E-Commerce/src/components/ButtonText.jsx
+++ 3legant-E-Commerce/src/components/ButtonText.jsx
@@ -5,7 +5,7 @@ import { arrowRight } from '../assets';
 const ButtonText = ({ text, linkTo }) => {
   return (
     <div className="flex justify-start">
-			<Link to={`/3legant-e-commerce/${linkTo}`} className="border-b-2 border-primary flex items-center">
+			<Link to={`/${linkTo}`} className="border-b-2 border-primary flex items-center">
 				<p className="font-inter font-medium text-primary text-sm">{text}</p>
 				<img src={arrowRight} alt="arrow right" className="ml-1" />
 			</Link>
