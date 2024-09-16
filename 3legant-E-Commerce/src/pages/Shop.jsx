59f7db63325f04c2f89843e2ec38e8ee598816f0
Toshak Parmar <108329884+toshakparmar@users.noreply.github.com>
2024-09-16 17:16:39 +0530

    Update Shop.jsx

--- 3legant-E-Commerce/src/pages/Shop.jsx
+++ 3legant-E-Commerce/src/pages/Shop.jsx
@@ -210,7 +210,7 @@ const Shop = () => {
               {products?.map((product) => (  
                 <MiniCard
                   product={product.name}
-                  image={`/3legant-e-commerce/${product.image}`}
+                  image={${product.image}}
                   price={product.price}
                   id={product._id}
                   oldPrice={product.price * 1.5}
