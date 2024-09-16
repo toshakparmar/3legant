a3d21cadfcdd28a45f67de6ff56c5ab75bf9d9ed
Toshak Parmar <108329884+toshakparmar@users.noreply.github.com>
2024-09-16 14:22:44 +0530

    Update SignIn.jsx

--- 3legant-E-Commerce/src/pages/SignIn.jsx
+++ 3legant-E-Commerce/src/pages/SignIn.jsx
@@ -31,7 +31,7 @@ const SignIn = () => {
 			handleError("Please provide all the information");
 		}
 		try{
-			const url = "https://threelegant.onrender.com/auth/login";
+			const url = "https://3legant-ten.vercel.app/auth/login";
 			const response = await fetch(url, {
 				method: "POST",
 				headers: {
